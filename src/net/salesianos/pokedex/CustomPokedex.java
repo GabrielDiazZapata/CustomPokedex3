afc2487c8f3272462ed32e00b02e9da5a62e720e
GabrielDiazZapata <126862544+GabrielDiazZapata@users.noreply.github.com>
2023-04-19 17:41:14 +0100

    feat(CustomPokedex, ListaInteligente, PokemonLegendario): En CustomPokedex se han llamado a los metodos de lista inteligente para formar un menu interactivo guardando los datos en ArrayLIst. En PokemonLegendario Se añadio un nuevo constructor

--- src/net/salesianos/pokedex/CustomPokedex.java
+++ src/net/salesianos/pokedex/CustomPokedex.java
@@ -1,5 +1,6 @@
 package net.salesianos.pokedex;
 import net.salesianos.pokemones.Pokemon;
+import net.salesianos.pokemones.PokemonLegendario;
 import java.util.ArrayList;
 import java.util.Scanner;
 public class CustomPokedex {
@@ -22,6 +23,77 @@ public class CustomPokedex {
             System.out.println("f) Cerrar la Pokédex");
             System.out.println("Selecciona una opción");
             opcion = teclado.nextLine();
+
+
+
+            switch (opcion) {
+                case "a":
+                System.out.print("Nombre del pokemon: ");
+                String nombre = teclado.nextLine();
+                System.out.print("Tipo del pokemon: ");
+                String tipo = teclado.nextLine();
+                Pokemon avistado = new Pokemon(nombre, tipo);
+                avistados.add(avistado);
+                System.out.println("¡Pokemon avistado añadido a la lista!");
+                break;
+
+                case "b":
+                System.out.print("Nombre del pokemon legendario: ");
+                String nombreLegendario = teclado.nextLine();
+                System.out.print("Tipo del pokemon legendario: ");
+                String tipoLegendario = teclado.nextLine();
+                System.out.print("Localización del pokemon legendario: ");
+                String localizacionLegendario = teclado.nextLine();
+                PokemonLegendario legendario = new PokemonLegendario(nombreLegendario, tipoLegendario);
+                legendario.setLocalizacion(localizacionLegendario);
+                avistados.add((Pokemon) legendario);
+                System.out.println("¡Avistamiento legendario añadido a la lista!");
+                break;
+
+
+                case "c":
+                    if (avistados.size() > 0) {
+                        System.out.println("Lista de Pokémon avistados:");
+                        for (int i = 0; i < avistados.size(); i++) {
+                            System.out.println((i + 1) + ") " + avistados.get(i));
+                        }
+                        System.out.print("Ingrese el número del Pokémon que desea capturar: ");
+                        int numPokemon = Integer.parseInt(teclado.nextLine());
+                        if (numPokemon > 0 && numPokemon <= avistados.size()) {
+                            Pokemon pokemonCapturado = avistados.remove(numPokemon - 1);
+                            capturados.add(pokemonCapturado);
+                            System.out.println("¡Has capturado a " + pokemonCapturado.getNombre() + "!");
+                        } else {
+                            System.out.println("Número inválido de Pokémon.");
+                        }
+                    } else {
+                        System.out.println("No hay Pokémon avistados.");
+                    }
+                    break;
+
+
+                case "d":
+                    System.out.println("\nLista de pokemones avistados:");
+                    for (Pokemon pokemon : avistados) {
+                        System.out.println("- " + pokemon.getNombre() + " (" + pokemon.getTipo() + ")");
+                        if (pokemon instanceof PokemonLegendario) {
+                            System.out.println("  Localización: " + ((PokemonLegendario) pokemon).getLocalizacion());
+                        }       
+                    }   
+                    break;
+
+
+                    case "e":
+                        System.out.println("\nLista de pokemones capturados:");
+                        for (Pokemon pokemon : capturados) {
+                            System.out.println("- " + pokemon.getNombre() + " (" + pokemon.getTipo() + ")");
+                            if (pokemon instanceof PokemonLegendario) {
+                                System.out.println("  Localización: " + ((PokemonLegendario) pokemon).getLocalizacion());
+                            }
+                        }
+                        break;
+
+            }
         }
     }
 }
