9d9955e48d737b2d4e29a44c8b441bd141d0ac83
GabrielDiazZapata <126862544+GabrielDiazZapata@users.noreply.github.com>
2023-04-19 09:59:09 +0100

    feat(PokemonLegendario) Creacion de Atributo, añadir constructores

---
+++ src/net/salesianos/pokemones/PokemonLegendario.java
@@ -0,0 +1,17 @@
+package net.salesianos.pokemones;
+
+public class PokemonLegendario extends Pokemon {
+    private String localizacion;
+
+    public PokemonLegendario(String nombre, String tipo, double altura, double peso, String descripcion, String localizacion) {
+        super(nombre, tipo, altura, peso, descripcion);
+        this.localizacion = localizacion;
+    }
+    
+
+    public PokemonLegendario(String nombre, String tipo, String localizacion) {
+        super(nombre, tipo);
+        this.localizacion = localizacion;
+    
+    }
+}
