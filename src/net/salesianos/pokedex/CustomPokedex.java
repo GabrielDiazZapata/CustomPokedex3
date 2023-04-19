025c4035e7ad8fe0f4e2951f096ec0ef3ff0499f
GabrielDiazZapata <126862544+GabrielDiazZapata@users.noreply.github.com>
2023-04-19 16:57:20 +0100

    feat(custom-pokedex): Añadir Imports, Creacion de 2 ArrayList, y un Menu interactivo

--- src/net/salesianos/pokedex/CustomPokedex.java
+++ src/net/salesianos/pokedex/CustomPokedex.java
@@ -1,5 +1,27 @@
 package net.salesianos.pokedex;
-
+import net.salesianos.pokemones.Pokemon;
+import java.util.ArrayList;
+import java.util.Scanner;
 public class CustomPokedex {
-    
+    public static void main(String[] args) throws Exception {
+        System.out.println("¡Bienvenido a CustomPokedex!");
+
+        ArrayList<Pokemon> avistados = new ArrayList<Pokemon>();
+        ArrayList<Pokemon> capturados = new ArrayList<Pokemon>();
+
+        Scanner teclado = new Scanner(System.in);
+        String opcion = "";
+
+        while (!opcion.equals("f")) {
+            System.out.println("\nMenu de interaccion:");
+            System.out.println("a) Añadir un pokemon avistado");
+            System.out.println("b) Añadir avistamiento legendario");
+            System.out.println("c) Pasar a capturados");
+            System.out.println("d) Mostrar lista de avistados");
+            System.out.println("e) Mostrar lista de caputrados");
+            System.out.println("f) Cerrar la Pokédex");
+            System.out.println("Selecciona una opción");
+            opcion = teclado.nextLine();
+        }
+    }
 }
