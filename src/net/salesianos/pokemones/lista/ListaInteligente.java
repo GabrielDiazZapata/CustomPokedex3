d092bfcf78c95ae2bb6e96b08141cf25c2674358
GabrielDiazZapata <126862544+GabrielDiazZapata@users.noreply.github.com>
2023-04-19 16:40:43 +0100

    feat(ListaInteligente): Imports añadidos, atributos y creacion de funcion agregarPokemon

--- src/net/salesianos/pokemones/lista/ListaInteligente.java
+++ src/net/salesianos/pokemones/lista/ListaInteligente.java
@@ -1,5 +1,25 @@
 package net.salesianos.pokemones.lista;
+import net.salesianos.pokemones.Pokemon;
+import java.util.Arrays;
 
 public class ListaInteligente {
-    
+    private Pokemon[] lista;
+    private int cantidad;
+
+public ListaInteligente() {
+    lista = new Pokemon[1];
+    cantidad = 0;
+}
+
+public void agregarPokemon(Pokemon p) {
+    if(cantidad == lista.length) {
+        lista = Arrays.copyOf(lista, lista.length * 2);
+    }
+    lista[cantidad] = p;
+    cantidad++;
+    System.out.println("Pokemon agregado correctamente gracias por nada bobo.");
+}
+
+
+
 }
