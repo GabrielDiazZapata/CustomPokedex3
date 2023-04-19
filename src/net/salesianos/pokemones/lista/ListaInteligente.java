bf981876886937e11799ef44ba16d5b1630dcba4
GabrielDiazZapata <126862544+GabrielDiazZapata@users.noreply.github.com>
2023-04-19 16:42:27 +0100

    feat(ListaInteligente): Añadidos 3 funciones para la clase,: eliminarPokemon, mostrarPokemon, getPokemon

--- src/net/salesianos/pokemones/lista/ListaInteligente.java
+++ src/net/salesianos/pokemones/lista/ListaInteligente.java
@@ -20,6 +20,35 @@ public void agregarPokemon(Pokemon p) {
     System.out.println("Pokemon agregado correctamente gracias por nada bobo.");
 }
 
+public void eliminarPokemon(String Nombre) {
+    for (int i = 0; i < cantidad; i++) {
+        if(lista[i].getNombre().equals(Nombre)) {
+            for(int j = i; j < cantidad - 1; j++) {
+                lista[j] = lista[j + 1];
+            }
+            cantidad--;
+            lista[cantidad] = null;
+            System.out.println("Pokemon eliminado correctamente ahora creame otro soplagaitas, y mas te vale que sea bueno.");
+            return;
+        }
+    }
+    System.out.println("No se encontró el Pokemon en la lista, aprende a escribir.");
+
+}
+
+public void mostrarPokemon() {
+    System.out.println("Lista de Pokemons:");
+    for (int i = 0; i < cantidad; i++) {
+        System.out.println((i+1) + ". " + lista[i].toString());
+    }
+}
+
+public Pokemon getPokemon(int posicion) {
+    if (posicion < 0 || posicion >= cantidad) {
+        return null;
+    }
+    return lista[posicion];
+    }
 
 
 }
